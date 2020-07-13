b25e78756041c58b4a7291e11affb8ddb69ae4e5
2DaeHee <40678414+leedh909@users.noreply.github.com>
2020-07-13 22:27:39 +0900

    2020.7.13

--- 실습자료/eclipse-servlet/Jsp11_SeoulBike/src/com/bike/controller/BikeServlet.java
+++ 실습자료/eclipse-servlet/Jsp11_SeoulBike/src/com/bike/controller/BikeServlet.java
@@ -1,6 +1,7 @@
 package com.bike.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.bike.dao.BikeDao;
 import com.bike.dto.BikeDto;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 @WebServlet("/bike.do")
 public class BikeServlet extends HttpServlet {
@@ -62,6 +66,72 @@ public class BikeServlet extends HttpServlet {
 				System.out.println("\ninsert 실패@!\n");
 				response.sendRedirect("bike01.jsp");
 			}
+		}else if(command.equals("second")) {
+			response.sendRedirect("bike02.jsp");
+		}else if(command.equals("second_db")) {
+			
+			int res = dao.delete();
+			if(res>0) {
+				System.out.println("삭제 성공!");
+			}else {
+				System.out.println("삭제 실패!!");
+			}
+			
+			String obj=request.getParameter("obj");
+			
+			JsonParser parser = new JsonParser();
+			JsonElement element = parser.parse(obj);
+			
+			
+			List<BikeDto> dtos = new ArrayList<BikeDto>();
+			
+			for(int i=0; i<element.getAsJsonObject().get("DATA").getAsJsonArray().size();i++) {
+//				System.out.println(element.getAsJsonObject().get("DATA").getAsJsonArray().get(i));
+				System.out.println(element.getAsJsonObject().get("DATA").getAsJsonArray().get(i).getAsJsonObject().get("new_addr"));
+				
+				JsonObject tmp = element.getAsJsonObject().get("DATA").getAsJsonArray().get(i).getAsJsonObject();
+				
+				JsonElement addr_gu_je = tmp.get("addr_gu");
+				JsonElement content_id_je = tmp.get("content_id");
+				JsonElement content_nm_je = tmp.get("content_nm");
+				JsonElement new_addr_je = tmp.get("new_addr");
+				JsonElement cradle_count_je = tmp.get("cradle_count");
+				JsonElement longitude_je = tmp.get("longitude");
+				JsonElement latitude_je = tmp.get("latitude");
+				
+				String addr_gu = addr_gu_je.getAsString();
+				int content_id = content_id_je.getAsInt();
+				String content_nm = content_nm_je.getAsString();
+				String new_addr = new_addr_je.getAsString();
+				int cradle_count = cradle_count_je.getAsInt();
+				double longitude = longitude_je.getAsDouble();
+				double latitude = latitude_je.getAsDouble();
+				
+				BikeDto dto = new BikeDto();
+				dto.setAddr_gu(addr_gu);
+				dto.setContent_id(content_id);
+				dto.setContent_nm(content_nm);
+				dto.setNew_addr(new_addr);
+				dto.setCradle_count(cradle_count);
+				dto.setLongitude(longitude);
+				dto.setLatitude(latitude);
+				
+				dtos.add(dto);
+				
+			}
+			
+			int result = dao.insert(dtos);
+			if(result >0) {
+				System.out.println("insert 성공\n");
+			}else {
+				System.out.println("insert 실패\n");
+			}
+			
+			PrintWriter out = response.getWriter();
+			out.print(result);
+			
+			
+			
 		}
 	}
 
@@ -70,3 +140,26 @@ public class BikeServlet extends HttpServlet {
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
